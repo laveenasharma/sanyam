bbe51a615c0a09a60c424485bece44eaa59debf0
unknown <laveena.sharma1@gmail.com>
2020-12-20 16:17:42 +0530

    platform created

--- sketch.js
+++ sketch.js
@@ -17,7 +17,7 @@ function setup(){
 
     
     ground = new Ground(600,height,1200,20)
-
+    platform= new Ground(150,300,300,200)
     box1 = new Box(700,320,70,70);
     box2 = new Box(920,320,70,70);
     pig1 = new Pig(810, 350);
@@ -39,6 +39,7 @@ function setup(){
 
 function draw(){
     background(backgroundImg);
+    strokeWeight(0);
     Engine.update(engine);
     console.log(box2.body.position.x);
     console.log(box2.body.position.y);
@@ -59,4 +60,5 @@ function draw(){
     log5.display();
 
     bird.display();
+    platform.display();
 }
